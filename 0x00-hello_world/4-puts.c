fcf170dc3b6d04e7e941c08a28d72b82c003d405
Eric Alaribe <ericsmithsonian2@gmail.com>
2022-03-11 00:54:46 +0100

    Completed task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
+/*
+*Task 4: Displays code to screen without using printf
+*/
 #include <stdio.h>
-int main() {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
-} 
+int main(void) 
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
